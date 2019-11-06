5d019819c92fca5a0a807198da0ebc24df1bf294
German <gcaballero@sinensia.com>
2019-11-06 15:52:04 +0100

    Empezando: HolaMundo.java

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,12 @@
+class HolaMundo {
+	public static void main(String[] argumentos) {
+		byte unByte = -128;
+		int unEntero = 1000;
+		
+		System.out.println("Hola Orense! Tipos de datos: " );
+		System.out.println("- Byte: " + unByte);
+		System.out.println("- Byte: " + unEntero);
+		
+		// return 0;
+	}
+}
