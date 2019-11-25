07db361664a2e4a913b76cff5bfd2095a46afa0e
German <gcaballero@sinensia.com>
2019-11-25 13:56:10 +0100

    COMIT INICIAL Práctica Modelo usuarios: - Clases ServicioUsuarios como clase gestionadora de usuarios. - Interfaz DAO - DAO usuario sin implemntar GenericDAO - Clase usuario con dos campos - Estructura base para los tests.

---
+++ 06_ModeloUsuarioBBDD/test/TestServicioUsuarios.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class TestServicioUsuarios {
+    
+    public TestServicioUsuarios() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void crearUsuariosInvalidos() {
+    }
+    @Test
+    public void crearUsuariosValidos() {
+    }
+    @Test
+    public void modificarUsuariosInvalidos() {
+    }
+    @Test
+    public void modificarUsuariosValidos() {
+    }
+    @Test
+    public void eliminarUsuarios() {
+    }
+}
