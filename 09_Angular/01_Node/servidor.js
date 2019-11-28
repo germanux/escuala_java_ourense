91846428f2fe7768b8d42e5560096c7b65681842
German <gcaballero@sinensia.com>
2019-11-28 18:04:07 +0100

    Empezando Angular, ejemplos de Node y de TypeScript.

---
+++ 09_Angular/01_Node/servidor.js
@@ -0,0 +1,11 @@
+console.log("Hola desde JS");
+
+console.log("Dir:      " + process.cwd());
+console.log("PID:      " + process.pid);
+console.log("Platform: " + process.platform);
+
+
+console.log("ppid:      " + process.ppid);
+
+
+while (true);
